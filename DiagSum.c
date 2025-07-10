3e4700d45ece04037f0d1bf9f92c65b1c9869b10
BedantaMaharana <bedantamaharana20@gmail.com>
2025-07-11 04:19:10 +0530

    Updated the check_prime() function in line 4 of DiagSum.c

--- DiagSum.c
+++ DiagSum.c
@@ -1,14 +1,24 @@
 #include <stdlib.h>   //malloc(), free() for dynamic memory allocation
 #include <stdio.h>    //printf(), scanf() for input/output operations
 #include <stdbool.h>  //for using bool type and true/false values
-bool check_prime(int num) { //Function for checking Prime number
-    if (num <= 1) return false;
-    if (num == 2) return true;
-    if (num % 2 == 0) return false;
-    for (int i = 3; i * i <= num; i += 2)
-        if (num % i == 0) return false;
+bool check_prime(int num) //function to check whether the number is prime or not
+{
+    int c = 0;
+    if(num <= 1)
+    return false;
+    
+    for(int i = 1;i <= num;i++)
+    {
+        if(num % i == 0)
+        c++;
+    }
+    if(c == 2)
     return true;
+    else
+    return false;
 }
+
+
 int compute_zigzag_sum(int** matrix, int n) { // function that computes the zigzag diagonal sum
     int sum = 0;
     for (int diag = 0; diag < 2 * n - 1; diag++) {
